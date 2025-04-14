2451d02a66b7c1ab370cf8d2b3270b5deeb193af
basedlsg <42946534+basedlsg@users.noreply.github.com>
2025-04-14 07:23:02 +0100

    Add files via upload

---
+++ generated/source/buildConfig/androidTest/debug/com/mowieinc/mowiekotlin/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mowieinc.mowiekotlin.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mowieinc.mowiekotlin.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String STRIPE_KEY = "pk_live_51Mj9dFGQVpwkvutOiqrm4a1sRMff7tzwwvrlmyBuvOZt6cfX5YqkxsBpOQ2c1vbl7nPNlJngFGG54ct1ZFxV3V0h00OGQedBpa";
+}
---
+++ generated/source/buildConfig/debug/com/mowieinc/mowiekotlin/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mowieinc.mowiekotlin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mowieinc.mowiekotlin";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String STRIPE_KEY = "pk_live_51Mj9dFGQVpwkvutOiqrm4a1sRMff7tzwwvrlmyBuvOZt6cfX5YqkxsBpOQ2c1vbl7nPNlJngFGG54ct1ZFxV3V0h00OGQedBpa";
+}
